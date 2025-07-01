67dab6c9e67e86150ffe22e39cba1c8420e4c219
Greg Willis <gwillis@bcap.com>
2025-06-30 17:24:09 -0700

    updated type for instance of the public client interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 "use client";
 
 import { useMsal, useIsAuthenticated } from "@azure/msal-react";
+import { IPublicClientApplication } from "@azure/msal-browser";
 import { loginRequest } from "@/config/authConfig";
 
-function handleLogin(instance) {
+function handleLogin(instance: IPublicClientApplication) {
     instance.loginRedirect(loginRequest).catch(e => {
         console.error(e);
     });
